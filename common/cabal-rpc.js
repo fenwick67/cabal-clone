991f33e958a581098b5fcecd83565010bc14d7b9
fenwick67 <smileman66@gmail.com>
2018-11-17 16:51:49 -0600

    add forgotten file

---
+++ common/cabal-rpc.js
@@ -0,0 +1,211 @@
+/*
+
+exports a server (and client) to be used with rpc.
+
+Create one for each cabal you open.
+
+Returns a wrapper 
+
+// create a server to handle all cabal requests
+require('cabal-rpc').Server()
+
+// create a client to send requests through
+var client = require('cabal-rpc').Client('Super Mario 64')
+
+
+Client methods: 
+
+client.sendMessage(channel,text,callback(er))
+client.getMessages(channel,[max],callback(messages,er))
+client.getChannels((channels,er)=>)
+client.getUser(callback(user,er))
+client.setUsername(name,er=>)
+
+events:
+
+client.on('message',(er,msg)=>
+
+
+*/
+
+const collect = require('collect-stream');
+
+function Server(...args){
+    var rpcServer = require('./rpc/get-server-channel');
+    if (args.length > 0){
+        rpcServer = rpcServer(...args);
+    }
+
+    var cabalKeyForString = require('./cabal-key-for-string');
+    var Cabal = require('cabal-core'); // NOTE: not a top-level require() because I don't want UI layer to call this
+    var swarm = require('cabal-core/swarm'); // same as above
+
+
+    var loadCabal = (plaintextKey,done)=>{
+        var key = cabalKeyForString(plaintextKey);
+
+        if (cabalCache[plaintextKey]){
+            return done(null,cabalCache[plaintextKey]);
+        }
+        let homedir = process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE;
+        let rootdir = homedir + '/.cabal-clone/archives/';
+        let dir = rootdir + key;
+
+        let cabal = Cabal(dir,key);
+
+        let ready = (er,cabal)=>{
+            if(er){
+                return done(er);
+            }else{
+                swarm(cabal);
+                cabalCache[plaintextKey] = cabal;
+                cabal.plaintextKey = plaintextKey;
+                return done(er,cabal);
+            }
+        }
+        
+        cabal.db.ready(()=>{
+            if(key){
+                ready(null,cabal);
+            }else{
+                cabal.getLocalKey((er,k)=>{
+                    ready(er,cabal);
+                });
+            }
+        });
+
+        cabal.on('message',function(...args){
+            rpcServer.emit('message:'+plaintextKey,...args)
+        })
+
+    };
+
+
+    var cabalCache = {}
+
+
+    // actual methods that will be exposed by the server
+
+    // send a message by text
+    rpcServer.registerAsyncHandler('sendMessage',function(plaintextKey,channel,text,done){
+        loadCabal(plaintextKey,(er,cabal)=>{
+            if(er){return done(er)}
+            var entry = {
+                type: 'chat/text',
+                content: {
+                  text: text,
+                  channel: channel.toLowerCase()
+                }
+              };
+            
+            cabal.publish(entry,done);
+        })
+    });
+
+    rpcServer.registerAsyncHandler('setNick',function(plaintextKey,nick,done){
+        loadCabal(plaintextKey,(er,cabal)=>{
+            if(er){return er(done)}
+            cabal.publishNick(nick,done);
+        });
+    });
+
+    rpcServer.registerAsyncHandler('getUser',function(plaintextKey,userid,done){
+        loadCabal(plaintextKey,(er,cabal)=>{
+            if(er){return er(done)}
+            cabal.users.get(userid,done);
+        });
+    });
+
+    rpcServer.registerAsyncHandler('getMessages',function(plaintextKey,channel,opts,done){
+        loadCabal(plaintextKey,(er,cabal)=>{
+            if(er){return er(done)}
+            var messageStream = cabal.messages.read(channel,opts);
+            collect(messageStream, done);
+        });
+    });
+
+    rpcServer.registerAsyncHandler('getChannels',function(plaintextKey,channel,opts,done){
+        
+        loadCabal(plaintextKey,(er,cabal)=>{
+            if(er){return er(done)}
+            cabal.channels.get(channel,opts,done);
+        });
+    });
+
+    rpcServer.registerAsyncHandler('getLocalKey',function(plaintextKey,done){
+        loadCabal(plaintextKey,(er,cabal)=>{
+            if(er){return er(done)}
+            cabal.getLocalKey(done);
+        });
+    });
+
+
+    return this;
+
+
+}
+
+class RemoteCabal extends require('events').EventEmitter {
+
+    constructor(plaintextKey){
+        super();
+        // connect to rpc Server
+        var cabalKeyForString = require('./cabal-key-for-string');
+        var rpcClient = require('./rpc/get-client-channel');
+
+        this.plaintextKey = plaintextKey;
+        this.key = cabalKeyForString(plaintextKey);
+
+        // generate functions that will call an rpc method with a cabal key as the first argument.
+        var generateKeyedProxyMethod = (name)=>{
+            return (...args)=>{
+                rpcClient.callMethod(name,this.key,...args);
+            }
+        }
+
+        this.sendMessage = generateKeyedProxyMethod('sendMessage');
+        this.setNick = generateKeyedProxyMethod('setNick');
+        this.getMessages = generateKeyedProxyMethod('getMessages');
+        this.getUser = generateKeyedProxyMethod('getUser');
+        this.getChannels = generateKeyedProxyMethod('getChannels');
+        this.getLocalKey = generateKeyedProxyMethod('getLocalKey');
+
+        rpcClient.on('message:'+plaintextKey,(...args)=>{
+            this.emit('message',...args);
+        })
+
+    }
+
+}
+
+exports.Server = Server;
+exports.RemoteCabal = RemoteCabal;
+exports.CabalClient = RemoteCabal;
+exports.Client = RemoteCabal;
+
+
+// helper 
+
+
+var randomKey = function(){
+	if(crypto.randomBytes){
+		return crypto.randomBytes(32).toString('hex');
+	}else if (crypto.getRandomValues){
+		return Array.prototype.slice.call(crypto.getRandomValues(new Uint8Array(32)))
+			.map(n=>{
+				var s = n.toString(16);
+				while(s.length < 2){s = '0'+s}
+			})
+			.join('');
+	}else{
+		var r = [];
+		for (var i = 0; i < 32; i ++){
+			r.push(Math.floor(256*Math.random()));
+		}
+		return r.map(n=>{
+			var s = n.toString(16);
+			while(s.length < 2){s = '0'+s}
+		})
+		.join('');
+	}
+}
