5e061e0fb6ce15589a04af40dadbc8ce996ab2d5
fenwick67 <smileman66@gmail.com>
2018-11-18 20:13:51 -0600

    update UI

--- dist.sh
+++ dist.sh
@@ -2,7 +2,5 @@
 
 rm -r dist
 
-./node_modules/.bin/electron-packager ./ --platform=darwin --out='dist'
 ./node_modules/.bin/electron-packager ./ --platform=linux --icon=desktop/icon --out='dist'
 ./node_modules/.bin/electron-packager ./ --platform=win32 --icon=desktop/icon --out='dist'
-./node_modules/.bin/electron-packager ./ --platform=mas --out='dist'
